a5949242f63b2c2dd7addf9ef59e45d493995da7
recharrs <sliz97028@gmail.com>
2018-05-27 21:16:43 +0800

    add testing

---
+++ a3c_single_pytorch/a3c_main_2.py
@@ -0,0 +1,108 @@
+import argparse
+import os
+import numpy as np
+import torch
+import torch.multiprocessing as mp
+
+from models import A3C_LSTM_GA
+from a3c_train import train
+from a3c_test import test
+
+import logging
+
+from my_utils import *
+
+parser = argparse.ArgumentParser(description='Gated-Attention for Grounding')
+
+# Environment arguments
+parser.add_argument('-l', '--max-episode-length', type=int, default=150,
+                    help='maximum length of an episode (default: 30)')
+parser.add_argument('-d', '--difficulty', type=str, default="hard",
+                    help="""Difficulty of the environment,
+                    "easy", "medium" or "hard" (default: hard)""")
+parser.add_argument('--living-reward', type=float, default=-0.005,
+                    help="""Default reward at each time step (default: 0,
+                    change to -0.005 to encourage shorter paths)""")
+parser.add_argument('--frame-width', type=int, default=300,
+                    help='Frame width (default: 300)')
+parser.add_argument('--frame-height', type=int, default=168,
+                    help='Frame height (default: 168)')
+parser.add_argument('-v', '--visualize', type=int, default=0,
+                    help="""Visualize the envrionment (default: 0,
+                    use 0 for faster training)""")
+parser.add_argument('--sleep', type=float, default=0,
+                    help="""Sleep between frames for better
+                    visualization (default: 0)""")
+parser.add_argument('--scenario-path', type=str, default="maps/room.wad",
+                    help="""Doom scenario file to load
+                    (default: maps/room.wad)""")
+parser.add_argument('--interactive', type=int, default=0,
+                    help="""Interactive mode enables human to play
+                    (default: 0)""")
+parser.add_argument('--all-instr-file', type=str,
+                    default="data/instructions_all.json",
+                    help="""All instructions file
+                    (default: data/instructions_all.json)""")
+parser.add_argument('--train-instr-file', type=str,
+                    default="data/instructions_train.json",
+                    help="""Train instructions file
+                    (default: data/instructions_train.json)""")
+parser.add_argument('--test-instr-file', type=str,
+                    default="data/instructions_test.json",
+                    help="""Test instructions file
+                    (default: data/instructions_test.json)""")
+parser.add_argument('--object-size-file', type=str,
+                    default="data/object_sizes.txt",
+                    help='Object size file (default: data/object_sizes.txt)')
+
+# A3C arguments
+parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
+                    help='learning rate (default: 0.001)')
+parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
+                    help='discount factor for rewards (default: 0.99)')
+parser.add_argument('--tau', type=float, default=1.00, metavar='T',
+                    help='parameter for GAE (default: 1.00)')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+
+parser.add_argument('--num-steps', type=int, default=20, metavar='NS',
+                    help='number of forward steps in A3C (default: 20)')
+
+parser.add_argument('-e', '--evaluate', type=int, default=0,
+                    help="""0:Train, 1:Evaluate MultiTask Generalization
+                    2:Evaluate Zero-shot Generalization (default: 0)""")
+                    
+parser.add_argument('--dump-location', type=str, default="./Asset/model/saved_2/model",
+                    help='path to dump models and log (default: ./saved/)')
+
+# Self-defined arguments
+parser.add_argument('--load', type=str, default="./Asset/model/saved_model",
+                    help='model path to load, 0 to not reload (default: 0)')
+parser.add_argument('-n', '--num-processes', type=int, default=8, metavar='N',
+                    help='how many training processes to use (default: 4)')
+parser.add_argument('--app-location', type=str, default='../Apps/kobuki_c2/kobuki_c2',
+                    help='location of appilication')
+parser.add_argument('--model-location', type=str, default='./Asset/model/saved_model',
+                    help='location of model')
+parser.add_argument('--video-location', type=str, default='./Asset/video',
+                    help='location of appilication')
+
+if __name__ == '__main__':
+    action = main(image, instruction)
+    
+def main(image, instruction):    
+    args = parser.parse_args()
+    args.input_size = len(env.word_to_idx)
+        
+    shared_model = A3C_LSTM_GA(args)
+    shared_model = shared_model.cuda()
+
+    # Load the model
+    if (args.load != "0"):
+        shared_model.load_state_dict(
+            torch.load(args.load, map_location=lambda storage, loc: storage))
+
+    shared_model.share_memory()
+    test_model = test(args, shared_model)
+    action = test_model.step(image, instruction)
+    return action
--- a3c_single_pytorch/a3c_test.py
+++ a3c_single_pytorch/a3c_test.py
@@ -11,135 +11,43 @@ from constants import *
 
 from Asset.my_utils import *
 
-def test(rank, args, shared_model):
-    torch.manual_seed(args.seed + rank)
+class test:
+    def __init__(self, args, share_model):
+        torch.manual_seed(args.seed + rank)
 
-    env = env_wrapper('Asset/Apps/kobuki_c2/kobuki_c2', idx=rank, base=128)
+        self.model = A3C_LSTM_GA(args)
+        self.model = model.cuda()
+
+        if (args.load != "0"):
+            print("Loading model ... "+ args.load)
+            self.model.load_state_dict(
+                torch.load(args.load, map_location=lambda storage, loc: storage))
+        model.eval()
+        
+        self.start = True
     
-    model = A3C_LSTM_GA(args)
-    model = model.cuda()
+    def step(image, instruction):
+        # Getting indices of the words in the instruction
+        instruction_idx = []
+        for word in instruction.split(" "):
+            instruction_idx.append(env.word_to_idx[word])
+        instruction_idx = np.array(instruction_idx)
 
-    if (args.load != "0"):
-        print("Loading model ... "+args.load)
-        model.load_state_dict(
-            torch.load(args.load, map_location=lambda storage, loc: storage))
+        image = torch.from_numpy(image).float()
+        instruction_idx = torch.from_numpy(instruction_idx).view(1, -1)
+        
+        if self.start:
+            self.cx = Variable(torch.zeros(1, 256), volatile=True)
+            self.hx = Variable(torch.zeros(1, 256), volatile=True)
+            self.start = False
 
-    model.eval()
-
-    (image, instruction), _, _, _ = env.reset()
-
-    # Print instruction while evaluating and visualizing
-    if args.evaluate != 0 and args.visualize == 1:
-        print("Instruction: {} ".format(instruction))
-
-    # Getting indices of the words in the instruction
-    instruction_idx = []
-    for word in instruction.split(" "):
-        instruction_idx.append(env.word_to_idx[word])
-    instruction_idx = np.array(instruction_idx)
-
-    image = torch.from_numpy(image).float()/255.0
-    instruction_idx = torch.from_numpy(instruction_idx).view(1, -1)
-
-    reward_sum = 0
-    done = True
-
-    start_time = time.time()
-
-    episode_length = 0
-    rewards_list = []
-    accuracy_list = []
-    episode_length_list = []
-    num_episode = 0
-    best_reward = 0.0
-    test_freq = 50
-    while True:
-        episode_length += 1
-        if done:
-            if (args.evaluate == 0):
-                model.load_state_dict(shared_model.state_dict())
-
-            cx = Variable(torch.zeros(1, 256), volatile=True)
-            hx = Variable(torch.zeros(1, 256), volatile=True)
-        else:
-            cx = Variable(cx.data, volatile=True)
-            hx = Variable(hx.data, volatile=True)
-
-        tx = Variable(torch.from_numpy(np.array([episode_length])).long(),
+        self.tx = Variable(torch.from_numpy(np.array([episode_length])).long(),
                       volatile=True)
 
-        value, logit, (hx, cx) = model(
+        value, logit, (self.hx, self.cx) = model(
                 (Variable(image.unsqueeze(0), volatile=True),
                  Variable(instruction_idx, volatile=True), (tx, hx, cx)))
         prob = F.softmax(logit)
         action = prob.max(1)[1].data.numpy()
 
-        (image, _), reward, done,  _ = env.step(action[0])
-
-        done = done or episode_length >= args.max_episode_length
-        reward_sum += reward
-
-        # export
-        if rank == 0 and num_episode % 10 == 0:
-            if done:
-                clip = make_plt_anim(frame, instruction_prev, reward)
-                clip.save("Asset/movie/ep_{:03d}.mp4".format(episode//10))
-                torch.save(model.state_dict(), './saved/model')
-                frame = []
-            else:
-                _image = np.swapaxes(image, 0, 2)
-                _image = np.swapaxes(_image, 0, 1)
-                frame.append(_image)
-        ##### export done
-
-        if done:
-            num_episode += 1
-            rewards_list.append(reward_sum)
-            # Print reward while evaluating and visualizing
-            if args.evaluate != 0 and args.visualize == 1:
-                print("Total reward: {}".format(reward_sum))
-
-            episode_length_list.append(episode_length)
-            if reward == CORRECT_OBJECT_REWARD:
-                accuracy = 1
-            else:
-                accuracy = 0
-            accuracy_list.append(accuracy)
-            if(len(rewards_list) >= test_freq):
-                print(" ".join([
-                    "Time {},".format(time.strftime("%Hh %Mm %Ss",
-                                      time.gmtime(time.time() - start_time))),
-                    "Avg Reward {},".format(np.mean(rewards_list)),
-                    "Avg Accuracy {},".format(np.mean(accuracy_list)),
-                    "Avg Ep length {},".format(np.mean(episode_length_list)),
-                    "Best Reward {}".format(best_reward)]))
-                logging.info(" ".join([
-                    "Time {},".format(time.strftime("%Hh %Mm %Ss",
-                                      time.gmtime(time.time() - start_time))),
-                    "Avg Reward {},".format(np.mean(rewards_list)),
-                    "Avg Accuracy {},".format(np.mean(accuracy_list)),
-                    "Avg Ep length {},".format(np.mean(episode_length_list)),
-                    "Best Reward {}".format(best_reward)]))
-                if np.mean(rewards_list) >= best_reward and args.evaluate == 0:
-                    torch.save(model.state_dict(),
-                               args.dump_location+"model_best")
-                    best_reward = np.mean(rewards_list)
-
-                rewards_list = []
-                accuracy_list = []
-                episode_length_list = []
-            reward_sum = 0
-            episode_length = 0
-            (image, instruction), _, _, _ = env.reset()
-            # Print instruction while evaluating and visualizing
-            if args.evaluate != 0 and args.visualize == 1:
-                print("Instruction: {} ".format(instruction))
-
-            # Getting indices of the words in the instruction
-            instruction_idx = []
-            for word in instruction.split(" "):
-                instruction_idx.append(env.word_to_idx[word])
-            instruction_idx = np.array(instruction_idx)
-            instruction_idx = torch.from_numpy(instruction_idx).view(1, -1)
-
-        image = torch.from_numpy(image).float()/255.0
+        return action
